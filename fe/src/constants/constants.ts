b95b2d8390b6823a631b4ec687abe7c16334f9ab
Fernando de Row <ferdr89dev@gmail.com>
2024-08-28 19:50:35 -0300

    update error messages in schema

---
+++ fe/src/constants/constants.ts
@@ -0,0 +1,7 @@
+export const validationFeedBack = {
+  required: "Esta campo es requerido",
+  moreThan: "El valor ingresado deber ser mayor a cero  ",
+  minNumber: "Debes ingresar al menos un valor mayor a cero",
+  minCharacters: "Debes ingresar al menos dos caracteres",
+  maxCharacters: "Debes ingresar menos de veinte caracteres",
+};
--- fe/src/schema/schema.ts
+++ fe/src/schema/schema.ts
@@ -1,30 +1,41 @@
+import { validationFeedBack } from "@/constants/constants";
 import * as yup from "yup";
 
-export const HomeFormSchema = yup
+export const userSchema = yup
   .object({
-    name: yup.string().min(2).max(20).required(),
+    name: yup
+      .string()
+      .min(2, validationFeedBack.minCharacters)
+      .max(20, validationFeedBack.maxCharacters)
+      .required(validationFeedBack.required),
     accountNumber: yup
       .number()
       .transform((value) => (isNaN(value) ? undefined : value))
-      .required("can not be empty")
-      .min(1)
+      .required(validationFeedBack.required)
+      .min(1, validationFeedBack.minNumber)
       .positive()
-      .moreThan(0),
-    balance: yup.number().positive().moreThan(0).required(),
+      .moreThan(0, validationFeedBack.moreThan),
+    balance: yup
+      .number()
+      .positive()
+      .moreThan(0, validationFeedBack.moreThan)
+      .required(validationFeedBack.required),
   })
   .required();
 
-export const DashBoardFormSchema = yup
-  .object({
-    cashInAmount: yup
-      .number()
-      .transform((value) => (isNaN(value) ? undefined : value))
-      .min(1)
-      .moreThan(0),
-    cashOutAmount: yup
-      .number()
-      .transform((value) => (isNaN(value) ? undefined : value))
-      .min(1)
-      .moreThan(0),
-  })
-  .required();
+export const cashInSchema = yup.object({
+  cash_in: yup
+    .number()
+    .transform((value) => (isNaN(value) ? undefined : value))
+    .required(validationFeedBack.required)
+    .min(1, validationFeedBack.minNumber)
+    .moreThan(0),
+});
+export const cashOutSchema = yup.object({
+  cash_out: yup
+    .number()
+    .transform((value) => (isNaN(value) ? undefined : value))
+    .required(validationFeedBack.required)
+    .min(1, validationFeedBack.minNumber)
+    .moreThan(0),
+});
