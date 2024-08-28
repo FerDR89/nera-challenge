4467e20e1ffc09f417fcd29b306b5138b797524b
Fernando de Row <ferdr89dev@gmail.com>
2024-08-28 17:57:30 -0300

    remove lib folder

--- fe/src/store/store.ts
+++ fe/src/store/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "../reducers/userSlice";
+import userReducer from "@/reducers/userSlice";
 
 export const makeStore = () =>
   configureStore({
